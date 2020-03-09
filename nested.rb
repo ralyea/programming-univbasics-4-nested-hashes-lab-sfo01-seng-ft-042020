f3bb05616258b9469f17ca2538b28ec5ace526d3
Paul Ralyea <047par@gmail.com>
2020-03-09 04:43:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,2 +1,153 @@
+def hopper
+	programmer_hash =
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
+programmer_hash[:grace_hopper][:known_for]= {:known_for => "COBOL", :languages => ["COBOL", "FORTRAN"]}
 
+
+end
+
+def alan_kay_is_known_for
+	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
+
+	programmer_hash =
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
+		 programmer_hash[:alan_kay][:known_for] = "Object Orientation"
+end
+
+def dennis_ritchies_language
+	programmer_hash =
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
+		dennis_ritchies_language = programmer_hash[:dennis_ritchie][:languages][0]
+		dennis_ritchies_language
+end
+
+def adding_matz
+# add the following information to the top level of programmer_hash
+# :yukihiro_matsumoto => {
+#   :known_for => "Ruby",
+#    :languages => ["LISP", "C"]
+# }
+
+	programmer_hash =
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
+
+		 new_programmer_hash = programmer_hash
+		 new_programmer_hash = {
+			 	 :yukihiro_matsumoto => {
+					 :known_for => "Ruby",
+					 :languages => ["LISP","C"]
+				 },
+
+         :grace_hopper => {
+           :known_for => "COBOL",
+           :languages => ["COBOL", "FORTRAN"]
+         },
+         :alan_kay => {
+           :known_for => "Object Orientation",
+           :languages => ["Smalltalk", "LISP"]
+         },
+         :dennis_ritchie => {
+           :known_for => "Unix",
+           :languages => ["C"]
+         }
+      }
+			new_programmer_hash
+
+end
+
+def changing_alan
+	programmer_hash =
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
+     #change what Alan Kay is :known_for to the value of the alans_new_info variable.
+     alans_new_info = "GUI"
+		 programmer_hash[:alan_kay][:known_for] = alans_new_info
+		 programmer_hash
+end
+
+def adding_to_dennis
+	programmer_hash =
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
+	programmer_hash[:dennis_ritchie][:languages][1]= "Assembly"
+	# programmer_hash[:dennis_ritchie][:languages].push("Assembly") #This line will also work the same as the line above(line 149)
+	programmer_hash
+
+end
 
