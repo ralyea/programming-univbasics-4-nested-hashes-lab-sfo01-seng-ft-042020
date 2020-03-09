1b3750eb5f2a8b9af5cb8ce2a8527e7a4cc68cf9
Paul Ralyea <047par@gmail.com>
2020-03-09 04:39:15 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -102,6 +102,22 @@ end
 
 
 def changing_alan
+  programmer_hash =
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     },
+  
   alans_new_info = "GUI"
 		 programmer_hash[:alan_kay][:known_for] = alans_new_info
 		 programmer_hash
