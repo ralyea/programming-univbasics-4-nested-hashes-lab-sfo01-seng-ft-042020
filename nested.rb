5446050124bfda6c8c1a05195d366602d2ef9285
Paul Ralyea <047par@gmail.com>
2020-03-09 04:36:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,10 +82,7 @@ def adding_matz
 					 :known_for => "Ruby",
 					 :languages => ["LISP","C"]
 				 },
-   programmer_hash[:yukihiro_matsumoto] = {
-   :known_for => "Ruby",
-   :languages => ["LISP", "C"]
-}
+  
 
 return programmer_hash
 end
